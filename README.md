830cfbb7b734e03fc5e11bf47b91fbe203b2d93d
Julian F <27401201+jkfar4@users.noreply.github.com>
2020-07-15 11:42:35 +1000

    updated read me

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Pong-Assignment
+
+The classic Pong game created using the Observable stream (coded in TypeScript).
+
+The pong.html, pong.ts and observable.ts is my work and the remaining files were supplied to help me get started.
+
+To play open pong.html
+
+
+
+
+
--- pong.ts
+++ pong.ts
@@ -1,11 +1,9 @@
-// FIT2102 2018 Assignment 1
-// https://docs.google.com/document/d/1woMAgJVf1oL3M49Q8N3E1ykTuTu5_r28_MQPVS5QIVo/edit?usp=sharing
 
 function pong() {
   // Inside this function you will use the classes and functions 
   // defined in svgelement.ts and observable.ts
   // to add visuals to the svg element in pong.html, animate them, and make them interactive.
-  // Study and complete the tasks in basicexamples.ts first to get ideas.
+
 
   // You will be marked on your functional programming style
   // as well as the functionality that you implement.
